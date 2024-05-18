3e9d481febc2fe194ea8d533bd7cb75d18e05ad6
Ahmed Hani <137453934+AhmedHaniAtef@users.noreply.github.com>
2024-05-18 04:07:50 +0300

    initial change

--- src/userprog/exception.c
+++ src/userprog/exception.c
@@ -148,6 +148,11 @@ page_fault (struct intr_frame *f)
   write = (f->error_code & PF_W) != 0;
   user = (f->error_code & PF_U) != 0;
 
+  if (validate_void_ptr(fault_addr))
+   syscall_exit(-1);
+  if(!write || !not_present || !user)
+   syscall_exit(-1);
+
   /* To implement virtual memory, delete the rest of the function
      body, and replace it with code that brings in the page to
      which fault_addr refers. */
@@ -158,4 +163,3 @@ page_fault (struct intr_frame *f)
           user ? "user" : "kernel");
   kill (f);
 }
-
--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -1,6 +1,13 @@
 #ifndef USERPROG_SYSCALL_H
 #define USERPROG_SYSCALL_H
 
+#include <stdbool.h>
+
 void syscall_init (void);
+
+/* check if the pointer is valid */
+bool validate_void_ptr(const void* pt);
+
+void syscall_exit(int status);
 
 #endif /* userprog/syscall.h */
